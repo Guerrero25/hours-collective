50bf47f27d0d5cb5e3527d9a16fa8e3b1ccb3ee0
Omar Guerrero <guerrero9725@gmail.com>
2019-07-09 16:57:47 -0500

    feat: Config project with firebase

--- android/app/src/main/java/com/hourscollective/MainApplication.java
+++ android/app/src/main/java/com/hourscollective/MainApplication.java
@@ -1,9 +1,11 @@
-package com.reactnativetemplateproject;
+package com.hourscollective;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,7 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGestureHandlerPackage()
+            new RNGoogleSigninPackage(),
+            new RNGestureHandlerPackage(),
+            new RNFirebasePackage()
       );
     }
 
